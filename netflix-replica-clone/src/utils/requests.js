35fce1f71b94c96cdecab4d4d3205099df3243cd
LalibBeyu <beyenetsegaye71@gmail.com>
2024-11-26 01:50:18 +0300

    axios and requests

---
+++ netflix-replica-clone/src/utils/requests.js
@@ -0,0 +1,14 @@
+
+const API_KEY = process.env.REACT_APP_API_KEY
+const requests ={
+    FetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    FetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    FetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    FetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    FetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    FetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    FetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    FetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    
+};
+export default requests
